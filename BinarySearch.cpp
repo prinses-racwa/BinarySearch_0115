624f1bff640e37a12a45a01335e263add01c4394
Rajwa Khoirunisa <rajwaamelia@gmail.com>
2026-04-16 10:35:08 +0700

    LanjutanProsedur1

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -21,4 +21,14 @@ void input()
             cout << "\n[i] Jumlah elemen tidak boleh lebih dari 10. Silakan coba lagi.\n";
         }
     }
+
+    cout << "\n=========================================\n";
+    cout << "           Masukkan Elemen Array           \n";
+    cout << "===========================================\n";
+
+    for (int i = 0; i < panjang; i++)
+    {
+        cout << "Data Ke-" << (i + 1) << " = ";
+        cin >> element[i];
+    }
 }
