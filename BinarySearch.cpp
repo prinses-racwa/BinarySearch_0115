39cf61e49b71a24c39fd5c92291b9d7769e1fc9a
Rajwa Khoirunisa <rajwaamelia@gmail.com>
2026-04-16 10:28:28 +0700

    Prosedur

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -4,3 +4,21 @@ using namespace std;
 int element[10];
 int nPanjang;
 int x;
+
+void input()   
+{
+    while (true)
+    {
+        cout << "Masukkan Elemen pada Array (Maksimal 10) : ";
+        cin >> nPanjang;
+
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[i] Jumlah elemen tidak boleh lebih dari 10. Silakan coba lagi.\n";
+        }
+    }
+}
