55e66f1920674b8eaafb5ab058bca0a0622da767
Rajwa Khoirunisa <rajwaamelia@gmail.com>
2026-04-16 11:04:41 +0700

    MenambahkanCentang

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -88,6 +88,14 @@ void binarySearch()
             int mid = (low + high) / 2;
 
             if (element[mid] == x)
+            {
+                cout << "\n[✓] Elemen " << x << " ditemukan pada indeks " << mid << "\n";
+                return;
+            }
+            if (x > element[mid])
+            {
+                low = mid + 1;
+            }
         }
     }
 }
